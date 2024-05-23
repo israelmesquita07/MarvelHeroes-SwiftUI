e1397a9421ed866c37a61c059df56f13722cce88
Israel Mesquita <israelmesquita07@gmail.com>
2024-05-22 22:15:14 -0300

    Initial Commit

---
+++ MarvelHeroes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarvelHeroes
+//
+//  Created by Israel Pinheiro Braga Mesquita on 22/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarvelHeroes/MarvelHeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelHeroesApp.swift
+//  MarvelHeroes
+//
+//  Created by Israel Pinheiro Braga Mesquita on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelHeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
